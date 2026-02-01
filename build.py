acedc13791303faffd0f9d53b03872201125b720
0x24a <miku399993@gmail.com>
2026-02-01 21:20:42 +0800

    feat: use loguru to log build process

--- build.py
+++ build.py
@@ -1,5 +1,6 @@
 from os import popen, path, mkdir, remove
 from shutil import rmtree, copytree, copy
+from loguru import logger
 from json import load
 
 # Read the lock file
@@ -9,38 +10,33 @@ with open('amiablog.lock', 'r') as f:
     lock = load(f)
     upstream_version = lock['version']
 
-print(f"Performing AmiaBlog build with version \"{upstream_version}\".")
+logger.info(f"Performing AmiaBlog build with version \"{upstream_version}\".")
 if path.exists("build"):
-    print("- Remove previous build artifacts")
+    logger.warning("Remove previous build artifacts")
     rmtree("build")
-print("- Creating build directory")
+logger.info("Creating build directory")
 mkdir("build")
-print("- Pulling upstream repository")
+logger.info("Pulling upstream repository")
 popen(f"git clone {upstream_url} build/AmiaBlog/").read()
 if upstream_version != "latest":
-    print("- Checking out specific version")
+    logger.info("Checking out specific version")
     popen(f"cd build/AmiaBlog && git checkout {upstream_version}").read()
-print("- Removing posts in the build directory")
+logger.info("Copying build assets")
 rmtree("build/AmiaBlog/posts")
-print("- Copying posts to the build directory")
 copytree("posts", "build/AmiaBlog/posts")
-print("- Removing attachments in the build directory")
 rmtree("build/AmiaBlog/attachments")
-print("- Copying attachments to the build directory")
 copytree("attachments", "build/AmiaBlog/attachments")
-print("- Removing config.json in the build directory")
 remove("build/AmiaBlog/config.json")
-print("- Copying config.json to the build directory")
 copy("config.json", "build/AmiaBlog/config.json")
-print("- Initializing virtual environment")
+logger.info("Initializing virtual environment")
 popen("cd build/AmiaBlog && uv sync").read()
-print("- Running static build")
+logger.info("Running static build")
 popen("cd build/AmiaBlog && uv run staticify.py").read()
 if path.exists("dist"):
-    print("- Removing previous distribution")
+    logger.warning("Removing previous distribution")
     rmtree("dist")
-print("- Copying back the distribution")
+logger.info("Copying back the distribution")
 copytree("build/AmiaBlog/dist", "dist")
-print("- Cleaning up")
+logger.info("Cleaning up")
 rmtree("build")
-print("Done")
+logger.info("Done")
